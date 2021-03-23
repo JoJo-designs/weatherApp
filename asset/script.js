5157d147acbb9b690968ddf745de9b4423f9d508
jojo <jodiladouceur.design@gmail.com>
2021-03-23 10:28:34 -0400

    added files

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,6 @@
-console.log("The script is connected")
+//Makeing sure script is connected
+console.log("The script is connected");
+
+//Grabing elements by id
+document.getElementById("searchCities").value;
+document.getElementById("searchHistory");
