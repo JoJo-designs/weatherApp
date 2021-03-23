6201db3eecdc149288be42efbd3d6bb093f8d125
jojo <jodiladouceur.design@gmail.com>
2021-03-23 11:18:50 -0400

    Edits toe README.md and script.js

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 The goal of this assigment is to bulid an app that will use the open weather api to inform the user about the weather in meny differant cities. It should have the ability to search for cities and display a wide range of data for the city. inclueing a 5 day forcast current conditions and the UV index.
 
 No starter files were provided for this assigment.
+
+The frist thing I completed was to make the elements I would needed in the html. 
+
+The next thing I want to get working is the search system. It needed to pull in the data for the searched city and save the name in local storage and create a new element in the search history section. I think I will try to get the value into local storage frist. I want to put the values in an array. After I able to save mulitple values to the array I will make a function that will loop the values and create a element for each one on the page. 
--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,33 @@
 //Makeing sure script is connected
 console.log("The script is connected");
 
+//Object to store names of searched cities
+var cityList = {
+    cities: [],
+}
+
 //Grabing elements by id
-document.getElementById("searchCities").value;
-document.getElementById("searchHistory");
+var searchValue = document.getElementById("searchCities").value;
+var searchHistory = document.getElementById("searchHistory");
+var searchButton = document.getElementById("search");
+
+
+searchButton.addEventListener("click", function(){
+    console.log("A button was clicked")
+    //cityList.cities = document.getElementById("searchCities").value;
+    cityList.cities.push(document.getElementById("searchCities").value)
+    console.log(cityList.cities);
+    localStorage.setItem("Cities", JSON.stringify(cityList.cities));
+});
+
+function buildHistoryElements() {
+    //localStorage.getItem( JSON.parse(cityList.cities));
+
+    //for (var i = 0; i < cityList.cities.length; i++) {
+       // element = cityList.cities[i];
+        
+        var createHisory = document.createElement("div") 
+        createHisory.textContent = localStorage.getItem( JSON.parse(cityList.cities));
+        document.searchHistory.appendChild(createHisory)
+    }
+//}
