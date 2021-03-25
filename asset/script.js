4cea874259d4fb2b172fc177f8b5f8d503a19a01
jojo <jodiladouceur.design@gmail.com>
2021-03-25 11:52:33 -0400

    Added more comments in script.js

--- asset/script.js
+++ asset/script.js
@@ -25,24 +25,28 @@ searchButton.addEventListener("click", function(){
     callApi();
 });
 
-//This is a funtion that was suppost to check if toronto was already in the array it work once than broke
+
+//This is a funtion that was suppost to check if toronto was already in the array 
+//It only works when their is already something in the array.
+//I wanted it to add something to the array so I guess this is not going to work but everthing else seems fine.
 function checkForToronto() {
   if (cities.indexOf("Toronto")){
      callStarterValue ();
    } else {
-    //commented out line make it so the value are not overwitten but makes it so Toronto is added over and over again.
+    //commented out line make it so the value are not overwitten but Toronto is added over and over again.
     //cities = JSON.parse(localStorage.getItem("Cities")) || [];
-    cities.push("Toronto")
     localStorage.setItem("Cities", JSON.stringify(cities));
+    cities.push("Toronto")
      callStarterValue ();
    }
 };
 
 
 
-//Calls a starter value so there is always data to display
+//Calls a starter value so there is always data to display.
+//Value is set to toronto
 function callStarterValue() {
-    //creates a value to start with
+    //creates a value to start with 
     var starterValue = "https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=89e0b7e8dbbac9434ed75176dac7f8a3&units=metric"
     var starterValueTwo = "https://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=89e0b7e8dbbac9434ed75176dac7f8a3&units=metric"
 
@@ -80,14 +84,13 @@ function callStarterValue() {
 
 
 
-
+//Calls the Data for user entered Locations
 function callApi(){
-    //I need to come up with a way to get the value of the input box and use it as part of the link.
     var searchValue = document.getElementById("searchCities").value.trim();
     var currentWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=89e0b7e8dbbac9434ed75176dac7f8a3&units=metric"
     var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=89e0b7e8dbbac9434ed75176dac7f8a3&units=metric"
 
-    //gets the current weather conditions
+    //gets the current weather conditions for user choosen city
         fetch(currentWeatherApi)
          .then(function (response) {
             if (response.ok) {
@@ -102,7 +105,7 @@ function callApi(){
             console.log('Unable to connect');
           });
 
-    //get five day weather
+    //et five day weather for User choosen city
           fetch(fiveDayApi)
          .then(function (response) {
             if (response.ok) {
@@ -118,7 +121,8 @@ function callApi(){
           });
 }
 
-// Function loop thought the values in local storage and builds and element for each.
+// Function loop thought the values in local storage and builds and element for each. it only runs after a refresh.
+// I need to make a function that build the elements that has just been added. 
 function buildHistoryElements() {
     console.log("this function is running")
     var location = localStorage.getItem("Cities");
@@ -137,8 +141,8 @@ function buildHistoryElements() {
     }
 }
 
-//callStarterValue();
 checkForToronto ();
 
-// API Link api.openweathermap.org/data/2.5/weather?q=  CITY NAME HERE  &appid=89e0b7e8dbbac9434ed75176dac7f8a3
+// Api Link "https://api.openweathermap.org/data/2.5/weather?q="  CITY NAME HERE  "&appid=89e0b7e8dbbac9434ed75176dac7f8a3" current weather
+// Api Link "https://api.openweathermap.org/data/2.5/forecast?q=" CITY NAME HERE  "&appid=89e0b7e8dbbac9434ed75176dac7f8a3" 5 day forcast
 //Key 89e0b7e8dbbac9434ed75176dac7f8a3
